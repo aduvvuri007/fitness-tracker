61897b2f7e800eefbe111d40d10a1e5708000329
Advik Duvvuri <advik.duvvuri@gmail.com>
2024-06-08 18:40:30 -0400

    created a server connection

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,10 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost:27017/fitness-login");
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Failed to connect to MongoDB Atlas', err));
