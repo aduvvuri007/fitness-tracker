a3565708c9c16d7e0f67574bbb7d571614f21163
Advik Duvvuri <advik.duvvuri@gmail.com>
2024-07-09 17:22:55 -0400

    fixed up  app.js to original

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import "./App.css";
 import Register from './pages/Register';
@@ -9,23 +9,19 @@ import Workout from './pages/Workout';
 import ExerciseList from './pages/ExerciseList';
 import ExerciseDetails from './pages/ExerciseDetails';
 
-const isAuthenticated = () => {
-  return !!localStorage.getItem('token'); // Check if token exists in localStorage
-};
-
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={!isAuthenticated() ? <Home /> : <Navigate to="/dashboard" />} />
-        <Route path="/register" element={!isAuthenticated() ? <Register /> : <Navigate to="/dashboard" />} />
-        <Route path="/login" element={!isAuthenticated() ? <Login /> : <Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={isAuthenticated() ? <Dashboard /> : <Navigate to="/login" />} />
-        <Route path="/workout" element={isAuthenticated() ? <Workout /> : <Navigate to="/login" />} />
-        <Route path="/exercises" element={isAuthenticated() ? <ExerciseList /> : <Navigate to="/login" />} />
-        <Route path="/exercises/:name" element={isAuthenticated() ? <ExerciseDetails /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/workout" element={<Workout />} />
+        <Route path="/exercises" element={<ExerciseList />} />
+        <Route path="/exercises/:name" element={<ExerciseDetails />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
